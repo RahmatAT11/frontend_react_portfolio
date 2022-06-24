6a082f9a317040831a1c94905657b520d6acc761
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2022-06-24 16:55:57 +0700

    add classname for styling the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,18 @@ import { images } from '../../constants';
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={images.logo} alt='logo'></img>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo'></img>
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
