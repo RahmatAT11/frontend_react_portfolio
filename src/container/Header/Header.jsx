cc22ecf3a28780b146fcae3d75575201dc286128
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2022-06-28 14:48:47 +0700

    finishing header section by implement style

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -16,7 +16,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div id='home' className='app__header app__flex'>
       <motion.div
         whileInView={{
           x: [-100, 0],
